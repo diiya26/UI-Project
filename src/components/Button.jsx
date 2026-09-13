92cbae18badc9feae8392835614f7d4f07ba7f80
Diya Prajapati <prajapatidia4@gmail.com>
2026-09-13 17:27:48 +0530

    initial commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+import react from 'react'
+const Button = () => {
+    return(
+        <div className='btn'>This is Button</div>
+    )
+}
+
+export default Button
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import react from 'react'
+const Header = () => {
+    return(
+        <div>Header</div>
+    )
+}
+
+export default Header
