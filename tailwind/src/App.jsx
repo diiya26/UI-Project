d3c81ded1f48de7b4008b5bb65a6bde9577747fe
Diya Prajapati <prajapatidia4@gmail.com>
2026-09-16 20:15:22 +0530

    updated ui-project project

--- tailwind/src/App.jsx
+++ tailwind/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Section1 from './components/Section1/Section1'
+import Section2 from './components/Section2/Section2'
 
 const App = () => {
   return (
-    <div className=''>App</div>
+    <Section1/>
+    <Section2/>
   )
 }
 
---
+++ tailwind/src/components/Section1/Section1.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Section1 = () => {
+    return(
+        <div className='h-screen w-full bg-blue-900'>Section1</div>
+        
+    )
+}
+export default Section1
